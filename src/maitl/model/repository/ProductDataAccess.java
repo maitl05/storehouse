15e797c1fa93bc0fe5669a0b0228076ace88ab3e
maitl05 <mahdiaminigm2@gmail.com>
2019-10-17 02:28:59 +0330

    product insert is implemented hopfully it's correct idk

--- src/maitl/model/repository/ProductDataAccess.java
+++ src/maitl/model/repository/ProductDataAccess.java
@@ -4,7 +4,9 @@ import maitl.model.common.ConnectionPool;
 import maitl.model.entity.Product;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class ProductDataAccess implements AutoCloseable {
     Connection connection;
@@ -16,7 +18,20 @@ public class ProductDataAccess implements AutoCloseable {
 
     }
     //insert select delete update search
-    public void insertProduct(Product product){
+    public void insertProduct(Product product) throws Exception{
+        preparedStatement = connection.prepareStatement("select product.nextval nid form dual");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        product.setProductID(resultSet.getLong("nid"));
+        product.setImportDate(new Date(System.currentTimeMillis()));
+        preparedStatement = connection.prepareStatement("insert into product (productID, importDate, productName, productCategory, price, productQuantityAvailableInStore) values (?,?,?,?,?,?)");
+        preparedStatement.setLong(1 , product.getProductID());
+        preparedStatement.setDate(2,product.getImportDate());
+        preparedStatement.setString(3,product.getProductName());
+        preparedStatement.setString(4,product.getProductCategory());
+        preparedStatement.setLong(5, product.getPrice());
+        preparedStatement.setLong(6,product.getProductQuantityAvailableInStore());
+        preparedStatement.executeUpdate();
 
 
     }
