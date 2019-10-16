b631745895537ea8e455c04d1ad8ab6a99de8863
maitl05 <mahdiaminigm2@gmail.com>
2019-10-17 01:50:39 +0330

    kir to gihub tokhmie to alan gayidi maro

--- src/maitl/model/entity/Product.java
+++ src/maitl/model/entity/Product.java
@@ -1,11 +1,77 @@
 package maitl.model.entity;
 
+import java.sql.Date;
+
 public class Product {
     private long productID;
     private String productName;
     private String productCategory;
     private long productQuantityAvailableInStore;
-    private long importDate;
+    private Date importDate;
     private long price;
 
+    public Product(){}
+
+    public Product(long productID, String productName, String productCategory, long productQuantityAvailableInStore, Date importDate, long price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.productCategory = productCategory;
+        this.productQuantityAvailableInStore = productQuantityAvailableInStore;
+        this.importDate = importDate;
+        this.price = price;
+    }
+
+    public long getProductID() {
+        return productID;
+    }
+
+    public Product setProductID(long productID) {
+        this.productID = productID;
+        return this;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public Product setProductName(String productName) {
+        this.productName = productName;
+        return this;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public Product setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+        return this;
+    }
+
+    public long getProductQuantityAvailableInStore() {
+        return productQuantityAvailableInStore;
+    }
+
+    public Product setProductQuantityAvailableInStore(long productQuantityAvailableInStore) {
+        this.productQuantityAvailableInStore = productQuantityAvailableInStore;
+        return this;
+    }
+
+    public Date getImportDate() {
+        return importDate;
+    }
+
+    public Product setImportDate(Date importDate) {
+        this.importDate = importDate;
+        return this;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public Product setPrice(long price) {
+        this.price = price;
+        return this;
+    }
 }
--- src/maitl/model/repository/ProductDataAccess.java
+++ src/maitl/model/repository/ProductDataAccess.java
@@ -1,4 +1,35 @@
 package maitl.model.repository;
 
-public class ProductDataAccess {
+import maitl.model.common.ConnectionPool;
+import maitl.model.entity.Product;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class ProductDataAccess implements AutoCloseable {
+    Connection connection;
+    PreparedStatement preparedStatement;
+
+    public ProductDataAccess() throws Exception{
+        connection = ConnectionPool.getConnection();
+        connection.setAutoCommit(false);
+
+    }
+    //insert select delete update search
+    public void insertProduct(Product product){
+
+
+    }
+
+
+
+
+
+
+    @Override
+    public void close() throws Exception {
+        connection.commit();
+        preparedStatement.close();
+        connection.close();
+    }
 }
