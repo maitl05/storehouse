e477921e1336160a84486cc60538cc2bfd8d081c
maitl05 <mahdiaminigm2@gmail.com>
2019-10-17 03:37:29 +0330

    product data access basically finished so many warnings lord please may this be working

--- src/maitl/model/repository/ProductDataAccess.java
+++ src/maitl/model/repository/ProductDataAccess.java
@@ -1,7 +1,10 @@
 package maitl.model.repository;
 
+
 import maitl.model.common.ConnectionPool;
 import maitl.model.entity.Product;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import java.sql.Connection;
 import java.sql.Date;
@@ -17,14 +20,14 @@ public class ProductDataAccess implements AutoCloseable {
         connection.setAutoCommit(false);
 
     }
-    //insert select delete update search
+    //delete update
     public void insertProduct(Product product) throws Exception{
         preparedStatement = connection.prepareStatement("select product.nextval nid form dual");
         ResultSet resultSet = preparedStatement.executeQuery();
         resultSet.next();
         product.setProductID(resultSet.getLong("nid"));
         product.setImportDate(new Date(System.currentTimeMillis()));
-        preparedStatement = connection.prepareStatement("insert into product (productID, importDate, productName, productCategory, price, productQuantityAvailableInStore) values (?,?,?,?,?,?)");
+        preparedStatement = connection.prepareStatement("insert into product (product_id, import_date, product_name, product_category, price, product_quantity_available_in_store) values (?,?,?,?,?,?)");
         preparedStatement.setLong(1 , product.getProductID());
         preparedStatement.setDate(2,product.getImportDate());
         preparedStatement.setString(3,product.getProductName());
@@ -36,9 +39,50 @@ public class ProductDataAccess implements AutoCloseable {
 
     }
 
+    public String selectAllProduct () throws Exception{
+        preparedStatement = connection.prepareStatement("select * from product");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        JSONArray jsonArray = new JSONArray();
+        while (resultSet.next()) {
+            jsonArray.add(one(resultSet));
+        }
+        return jsonArray.toJSONString();
+    }
 
+    public String selectOneProduct(Long productID) throws Exception{
+        preparedStatement = connection.prepareStatement("select * from product where product_id = ?");
+        preparedStatement.setLong(1, productID);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        return one(resultSet).toJSONString();
 
+    }
 
+    private JSONObject one (ResultSet resultSet) throws Exception{
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("productID", resultSet.getString("product_id"));
+        jsonObject.put("importDate", resultSet.getString("import_date"));
+        jsonObject.put("productName", resultSet.getString("product_name"));
+        jsonObject.put("productCategory", resultSet.getString("product_category"));
+        jsonObject.put("price", resultSet.getLong("price"));
+        jsonObject.put("ProductQuantityAvailableInStore",resultSet.getLong("product_quantity_available_in_store"));
+        return jsonObject;
+    }
+
+    public void updateProduct(Product product) throws Exception{
+        preparedStatement  = connection.prepareStatement("update product set price = ?, product_quantity_available_in_store = ? where product_id=?");
+        preparedStatement.setLong(1, product.getPrice());
+        preparedStatement.setLong(2,product.getProductQuantityAvailableInStore());
+        preparedStatement.setLong(3,product.getProductID());
+        preparedStatement.executeUpdate();
+
+    }
+
+    public void deleteProduct(long productID)throws Exception{
+        preparedStatement  = connection.prepareStatement("delete from product where product_id=?");
+        preparedStatement.setLong(1,productID);
+        preparedStatement.executeUpdate();
+    }
 
 
     @Override
