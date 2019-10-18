8ea94058d62740dd9b318531e9d905a87387be53
maitl05 <mahdiaminigm2@gmail.com>
2019-10-19 02:14:24 +0330

    fix general deleted the whitespaces u sensitive fuck

--- src/maitl/model/service/SalesListService.java
+++ src/maitl/model/service/SalesListService.java
@@ -1,10 +1,18 @@
 package maitl.model.service;
 
+import maitl.model.entity.Product;
 import maitl.model.entity.SalesList;
+import maitl.model.repository.ProductDataAccess;
 import maitl.model.repository.SalesListDataAccess;
-import org.codehaus.jettison.json.JSONString;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+class notEnoughProductAvailable extends Exception{
+    public notEnoughProductAvailable(){
+        super("not enough products available in storehouse");
+    }
+}
 
 public class SalesListService {
     private static SalesListService salesListService= new SalesListService();
@@ -20,17 +28,28 @@ public class SalesListService {
             String stringProduct = ProductService.getProductServiceInstance().showOneProduct(salesList.getProductID());
             JSONParser parser = new JSONParser();
             JSONObject jsonProduct = (JSONObject) parser.parse(stringProduct);
-            Object object = jsonProduct.get("ProductQuantityAvailableInStore");
+            long productQuantity = Long.parseLong(jsonProduct.get("ProductQuantityAvailableInStore").toString());
 
+            if(productQuantity < salesList.getSalesQuantity()){
+                throw new notEnoughProductAvailable();
+            }
+            Product product = new Product().setProductID(salesList.getProductID()).setProductQuantityAvailableInStore(productQuantity - salesList.getSalesQuantity());
+            ProductDataAccess productDataAccess = new ProductDataAccess();
+            productDataAccess.updateProduct(product);
+            salesListDataAccess.insertSalesList(salesList);
         }
-
-
     }
 
+    public String showAllSale()throws Exception{
+        try(SalesListDataAccess salesListDataAccess = new SalesListDataAccess()){
+            return salesListDataAccess.selectAllSalesList();
+        }
+    }
 
-
-
-
-
+    public  String showOneProductSale(long productID) throws Exception{
+        try(SalesListDataAccess salesListDataAccess = new SalesListDataAccess()){
+            return salesListDataAccess.selectOneProductFromSalesList(productID);
+        }
+    }
 
 }
