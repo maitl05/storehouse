97bc3d5278e34a0e7eb1ab077a529d111da62f21
Milkh <ir.milkh@gmail.com>
2019-10-22 06:11:04 +0330

    updated NotEnoughProductAvailableException to contain more info

--- src/main/java/maitl/model/repository/NotEnoughProductAvailableException.java
+++ src/main/java/maitl/model/repository/NotEnoughProductAvailableException.java
@@ -4,4 +4,7 @@ public class NotEnoughProductAvailableException extends Exception{
     public NotEnoughProductAvailableException(){
         super("not enough products available in storehouse");
     }
+    public NotEnoughProductAvailableException(Long productID){
+        super("not enough products available for product number "+productID+" in storehouse");
+    }
 }
